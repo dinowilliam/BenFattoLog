e5f1b9338b62d213060d37c32b9f5bc966f6f9c9
William Ferreira de Paula <dinowilliam@gmail.com>
2020-12-11 21:33:21 -0300

    Changes:    -Add Bootstrap from libman    -Add fonte awesome from libman   - Add Bootstrap Table from libman   - Add Jquery from libman   - Add Bootstrap Table to ManualLog   - Add manual.log.js   - Changes in Layout.cshtml to fix paths to librarys

--- BenFattoLog.UI/Controllers/HomeController.cs
+++ BenFattoLog.UI/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace BenFattoLog.UI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ManualLog()
         {
             return View();
         }
